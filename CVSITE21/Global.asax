a7bfe728a6227d9adac9ea20312404679aa788f0
stephanie <stephanie.maqdis@icloud.com>
2021-12-20 14:49:43 +0100

    Add project files.

---
+++ CVSITE21/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CVSITE21.MvcApplication" Language="C#" %>
---
+++ CVSITE21/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CVSITE21.Startup))]
+namespace CVSITE21
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
