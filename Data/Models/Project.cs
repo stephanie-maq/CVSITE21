066071fd817e79d7dac7bc2ef3438b45c87fc397
marcus P <mejl2marcuspahlsson@hotmail.com>
2021-12-21 17:44:26 +0100

    Skrivit project och user model

--- Data/Models/Project.cs
+++ Data/Models/Project.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace Data.Models
 {
     internal class Project
-    {
+    {   
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public string ImagePath { get; set; }
+
     }
 }
--- Data/Models/User.cs
+++ Data/Models/User.cs
@@ -8,5 +8,8 @@ namespace Data.Models
 {
     internal class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
     }
 }
