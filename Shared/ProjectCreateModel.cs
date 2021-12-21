c79d478642a52dbecd702f97aec872a54e1ae2ca
marcus P <mejl2marcuspahlsson@hotmail.com>
2021-12-21 18:14:47 +0100

    Lagt till shared, controller till project, och strukturerat mer.

---
+++ Shared/ProjectCreateModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Shared
+{
+    public class ProjectCreateModel
+    {
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public HttpPostedFileBase Image { get; set; }
+
+    }
+}
