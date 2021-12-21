61d69fedf875e088278acb220541beade59ff8b3
stephanie <stephanie.maqdis@icloud.com>
2021-12-22 00:50:49 +0100

    added description,title properties to project.cs

--- Data/Models/CV.cs
+++ Data/Models/CV.cs
@@ -8,5 +8,6 @@ namespace Data.Models
 {
     internal class CV
     {
+
     }
 }
--- Data/Models/Project.cs
+++ Data/Models/Project.cs
@@ -7,9 +7,11 @@ using System.Threading.Tasks;
 namespace Data.Models
 {
     internal class Project
-    {   
+    {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Title { get; set; }
+
+        public string Description { get; set; }
         public DateTime Created { get; set; }
         public string ImagePath { get; set; }
 
