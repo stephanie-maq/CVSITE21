cc82be4c3aafedd65ad06106955f86fb286647fe
Stephanie Maqdis <84038847+stephanie-maq@users.noreply.github.com>
2021-12-29 21:46:26 +0100

    Revert "Profile design"

--- Data/Migrations/202112292016397_init.Designer.cs
+++ Data/Migrations/202112292016397_init.Designer.cs
@@ -7,13 +7,13 @@ namespace Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.4.4")]
-    public sealed partial class CVMigration : IMigrationMetadata
+    public sealed partial class init : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(CVMigration));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(init));
         
         string IMigrationMetadata.Id
         {
-            get { return "202112291917160_init"; }
+            get { return "202112292016397_init"; }
         }
         
         string IMigrationMetadata.Source
