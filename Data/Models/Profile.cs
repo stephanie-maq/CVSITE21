132948ae9e15cde4bd4153fb5f6b67fdf03bb293
stephanie <stephanie.maqdis@icloud.com>
2021-12-29 22:09:08 +0100

    Rename and delete

---
+++ Data/Models/Profile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    class Profile
+    {
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public string Experience { get; set; }
+        public string Education { get; set; }
+        public string Skills { get; set; }
+        public string Languages { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
