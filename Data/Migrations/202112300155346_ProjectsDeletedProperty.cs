021c61cf35612c332817b1a3adef3e919a3523bd
Ike <ike.okoroha@hotmail.com>
2021-12-30 03:00:53 +0100

    Tog bort ImagePath propertyn från Project model och från vyerna.

---
+++ Data/Migrations/202112300155346_ProjectsDeletedProperty.cs
@@ -0,0 +1,22 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ProjectsDeletedProperty : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Projects", "Title", c => c.String(nullable: false));
+            AlterColumn("dbo.Projects", "Description", c => c.String(nullable: false));
+            DropColumn("dbo.Projects", "ImagePath");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Projects", "ImagePath", c => c.String());
+            AlterColumn("dbo.Projects", "Description", c => c.String());
+            AlterColumn("dbo.Projects", "Title", c => c.String());
+        }
+    }
+}
