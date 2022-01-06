da73a7b1d0e982463dd3619334f4872d5661ed1d
stephanie <stephanie.maqdis@icloud.com>
2022-01-07 00:21:33 +0100

    Wip

--- Data/Models/Message.cs
+++ Data/Models/Message.cs
@@ -12,7 +12,7 @@ namespace Data.Models
     {
         [Key]
         public int Id { get; set; }
-        public string Sender { get; set; }
+        public string From { get; set; }
         public string Text { get; set; }
         public string Recipient { get; set; }
         public bool IsRead { get; set; }
