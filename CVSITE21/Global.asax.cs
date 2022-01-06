edfddd8795e33e2dc33279ba60b18bb9690d3984
stephanie <stephanie.maqdis@icloud.com>
2022-01-06 19:31:44 +0100

    Installed WebApi and added WebApiConfig

--- CVSITE21/Global.asax.cs
+++ CVSITE21/Global.asax.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using CVSITE21.App_Start;
 
 namespace CVSITE21
 {
@@ -16,6 +18,7 @@ namespace CVSITE21
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }
---
+++ Data/Models/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Message
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Sender { get; set; }
+        public string Text { get; set; }
+        public string Recipient { get; set; }
+        public bool IsRead { get; set; }
+
+    }
+}
