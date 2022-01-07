21ce0c9894a09f17fe76e020e1db7a3b89122316
stephanie <stephanie.maqdis@icloud.com>
2022-01-07 01:49:27 +0100

    Messages View

--- Data/Models/Message.cs
+++ Data/Models/Message.cs
@@ -12,10 +12,12 @@ namespace Data.Models
     {
         [Key]
         public int Id { get; set; }
-        public string From { get; set; }
+        [Display(Name = "From")]
+        public string Sender { get; set; }
         public string Text { get; set; }
         public string Recipient { get; set; }
-        public bool IsRead { get; set; }
+        [Display(Name = "Status")]
+        public bool isRead { get; set; }
 
     }
 }
