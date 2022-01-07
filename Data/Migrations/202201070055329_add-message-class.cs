a077ac470ab7d4f89f256751b26d632a89d65cd8
stephanie <stephanie.maqdis@icloud.com>
2022-01-07 01:56:25 +0100

    Added messages to db

---
+++ Data/Migrations/202201070055329_add-message-class.cs
@@ -0,0 +1,29 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addmessageclass : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Messages",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Sender = c.String(),
+                        Text = c.String(),
+                        Recipient = c.String(),
+                        isRead = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Messages");
+        }
+    }
+}
