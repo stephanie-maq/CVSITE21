d7eda3310b15008c098897036d0a1c52195fafae
stephanie <stephanie.maqdis@icloud.com>
2022-01-07 02:22:26 +0100

    added webApiConfig.cs and message.cs along with two controllers, and a message view

---
+++ CVSITE21/Controllers/MessageController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CVSITE21.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Message/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Message/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Message/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Message/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Message/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Message/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Message/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
