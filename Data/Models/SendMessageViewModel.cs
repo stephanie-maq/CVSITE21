90cb267b26c9f7efd013a75d1759398bbfb388c2
marcus P <mejl2marcuspahlsson@hotmail.com>
2022-01-09 23:49:02 +0100

    Fixar det Steph tog bort..........

---
+++ Data/Models/SendMessageViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class SendMessageViewModel
+    {
+        public Profile Profile { get; set; }
+        public Message Message { get; set; }
+        public string Sender { get; set; }
+
+
+        public SendMessageViewModel(Profile profile, string sender)
+        {
+            this.Profile = profile;
+            this.Sender = sender;
+        }
+        public SendMessageViewModel(Profile profile, Message message, string sender)
+        {
+            this.Profile = profile;
+            this.Message = message;
+            this.Sender = sender;
+        }
+        public SendMessageViewModel()
+        {
+
+        }
+    }
+}
