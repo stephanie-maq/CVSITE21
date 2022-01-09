040c3ecf92d4b0aef65324cff1f2d30d1ce41fa3
stephanie <stephanie.maqdis@icloud.com>
2022-01-09 16:10:03 +0100

    api controller done

--- CVSITE21/Controllers/MessageApiController.cs
+++ CVSITE21/Controllers/MessageApiController.cs
@@ -4,36 +4,31 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using CVSITE21.Data;
+using Data.Models;
 
 namespace CVSITE21.Controllers
 {
     public class MessageApiController : ApiController
     {
-        // GET api/<controller>
-        public IEnumerable<string> Get()
+        [HttpPost]
+        [Route("api/messages/{sender}/{receiver}/{text}")]
+        public IHttpActionResult SendMessage(string sender, string receiver, string text)
         {
-            return new string[] { "value1", "value2" };
-        }
+            using (var context = new ApplicationDbContext())
+            {
+                var message = new Message
+                {
+                    Sender = sender,
+                    Recipient = receiver,
+                    Text = text,
+                    isRead = false
+                };
+                context.Messages.Add(message);
+                context.SaveChanges();
+            }
 
-        // GET api/<controller>/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/<controller>
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<controller>/5
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<controller>/5
-        public void Delete(int id)
-        {
+            return Ok();
         }
     }
 }
