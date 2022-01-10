ca5dd2504de2088f68083ba0384a911f7341f54f
marcus P <mejl2marcuspahlsson@hotmail.com>
2022-01-10 18:53:29 +0100

    fixa sista validation och readme

--- Data/Models/Profile.cs
+++ Data/Models/Profile.cs
@@ -14,7 +14,9 @@ namespace Data.Models
 
         [DataType(DataType.EmailAddress, ErrorMessage = "E-mail is not valid")]
         public string Email { get; set; }
+        [RegularExpression(@"^[A-Öa-ö ]+$", ErrorMessage = "Names can only have letters and space")]
         public string Fullname { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9 ]+$", ErrorMessage = "Adress can only have letters, numbers and space")]
         public string Address { get; set; }
         public int Age { get; set; }
         public string ImagePath { get; set; }
